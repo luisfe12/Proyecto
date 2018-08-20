2dc8952d9a725ad198f437c191bb123e7f9ccfa9
Sorbete-Sempai123 <tipon_tn@hotmail.com>
2018-08-20 18:01:17 -0500

    El Trabajo Real u.u

---
+++ suma_2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int a,b;
+    cout<<"Ingrese los 2 numeros que desea sumar"<<endl;
+    cin>>a>>b;
+    cout<<a+b<<" Yober Mendoza Surco"<<endl;
+    return 0;
+}
